0adb9bd0f364fa99189ece386738446ae875ce36
ShahiemDeca <69846601+ShahiemDeca@users.noreply.github.com>
2024-01-12 17:22:54 +0100

    feat: add walking

--- src/engine/entity/Character.ts
+++ src/engine/entity/Character.ts
@@ -2,10 +2,27 @@ import Entity from './Entity';
 import { Assets, AnimatedSprite, Rectangle, Texture, BaseTexture } from 'pixi.js';
 
 export default class Character extends Entity {
-  assets: any[] = [];
+  private keys: { [key: string]: boolean } = {};
+  private sprite: any;
+  private walkingSpeed: number = 5;
 
   constructor() {
     super();
+
+    this.setupKeyboardListeners();
+  }
+
+  private setupKeyboardListeners() {
+    window.addEventListener('keydown', this.handleKeyDown.bind(this));
+    window.addEventListener('keyup', this.handleKeyUp.bind(this));
+  }
+
+  private handleKeyDown(event: KeyboardEvent) {
+    this.keys[event.key] = true;
+  }
+
+  private handleKeyUp(event: KeyboardEvent) {
+    this.keys[event.key] = false;
   }
 
   async preload() {
@@ -35,12 +52,25 @@ export default class Character extends Entity {
 
     const animatedSprite = new AnimatedSprite(frames);
     animatedSprite.animationSpeed = 0.05;
-    animatedSprite.scale.set(2);
     animatedSprite.play();
 
+    this.sprite = animatedSprite;
     stage.addChild(animatedSprite);
   }
 
+  private flipSprite() {
+    this.sprite.scale.x *= -1;
+  }
+
   public update(delta: number) {
+    if (this.keys['ArrowLeft']) {
+      this.sprite.x -= this.walkingSpeed * delta;
+      if (this.sprite.scale.x > 0) this.flipSprite();
+    }
+
+    if (this.keys['ArrowRight']) {
+      this.sprite.x += this.walkingSpeed * delta;
+      if (this.sprite.scale.x < 0) this.flipSprite();
+    }
   }
 }
